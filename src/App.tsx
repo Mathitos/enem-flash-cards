545bcbc5d46f1a8546f2362942d7d62b97bcfdda
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2022-10-26 08:16:31 -0300

    add flip card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import './app.scss';
+import React, { ReactNode, useState } from 'react'
+import './app.scss'
 
 function App() {
   return (
     <div className="app-container">
-
+      <FlipCard
+        frontContent={<p>Qual o Teorema de Pitágoras?</p>}
+        backContent={
+          <div>
+            <p>
+              A soma dos quadrados de seus catetos corresponde ao
+              quadrado de sua hipotenusa
+            </p>
+            <br />
+            <strong>a² = b² + c²</strong>
+          </div>
+        }
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+const FlipCard: React.FC<{
+  frontContent: ReactNode
+  backContent: ReactNode
+}> = ({ frontContent, backContent }) => {
+  const [isFlipped, setIsFlipped] = useState<boolean>(false)
+  return (
+    <div
+      className={
+        isFlipped ? 'flip-card' : 'flip-card flip-card--flipped '
+      }
+      onClick={() => setIsFlipped(!isFlipped)}
+    >
+      <div className="flip-card-inner">
+        <div className="flip-card-front">{frontContent}</div>
+        <div className="flip-card-back">{backContent}</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
