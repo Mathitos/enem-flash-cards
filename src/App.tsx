248c1999f27725a9413a9892d30f9bb51b5f7201
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2022-10-27 16:07:00 -0300

    add firebase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,59 @@
-import React, { ReactNode, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './app.scss'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { collection, query, where, getDocs } from 'firebase/firestore'
 
-function App() {
+const firebaseConfig = {
+  apiKey: 'AIzaSyB5RSYKvUPsSaOWzXNSveQ3S_Lg678D6lM',
+  authDomain: 'flash-cards-95e03.firebaseapp.com',
+  projectId: 'flash-cards-95e03',
+  storageBucket: 'flash-cards-95e03.appspot.com',
+  messagingSenderId: '491480215396',
+  appId: '1:491480215396:web:20f245a02ee20be1d6d471',
+  measurementId: 'G-W9897GM6SY',
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+type CardData = {
+  id: string
+  frontData: string
+  backData: string
+}
+
+const App = () => {
+  const [cards, setCards] = useState<Array<CardData>>([])
+
+  useEffect(() => {
+    const foo = async () => {
+      const q = query(collection(db, 'cards'))
+
+      const querySnapshot = await getDocs(q)
+      const cardsResult = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        frontData: doc.data().frontData,
+        backData: doc.data().backData,
+      }))
+      setCards(cardsResult)
+    }
+
+    foo()
+  }, [])
+
   return (
     <div className="app-container">
-      <FlipCard
-        frontContent={<p>Qual o Teorema de Pitágoras?</p>}
-        backContent={
-          <div>
-            <p>
-              A soma dos quadrados de seus catetos corresponde ao
-              quadrado de sua hipotenusa
-            </p>
-            <br />
-            <strong>a² = b² + c²</strong>
-          </div>
-        }
-      />
+      {cards.map((card) => (
+        <FlipCard key={card.id} data={card} />
+      ))}
     </div>
   )
 }
 
 const FlipCard: React.FC<{
-  frontContent: ReactNode
-  backContent: ReactNode
-}> = ({ frontContent, backContent }) => {
+  data: CardData
+}> = ({ data }) => {
   const [isFlipped, setIsFlipped] = useState<boolean>(false)
   return (
     <div
@@ -34,8 +63,8 @@ const FlipCard: React.FC<{
       onClick={() => setIsFlipped(!isFlipped)}
     >
       <div className="flip-card-inner">
-        <div className="flip-card-front">{frontContent}</div>
-        <div className="flip-card-back">{backContent}</div>
+        <div className="flip-card-front">{data.frontData}</div>
+        <div className="flip-card-back">{data.backData}</div>
       </div>
     </div>
   )
